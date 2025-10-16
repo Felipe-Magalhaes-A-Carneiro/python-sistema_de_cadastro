64e8c3e67baf1e4516966a68bb649f9726031fb7
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2025-10-16 16:10:59 -0300

    feat(): condicao para se não houver um registro

--- registro_emprestimos.py
+++ registro_emprestimos.py
@@ -7,13 +7,17 @@ def registros_realizados():
     |======= SISTEMA DE CADASTRAMENTO DE LIVROS =======|
     ----------------------------------------------------
     
-    >>> REGISTROS REALIZADOS:
-    """)
-            
-    for cadastro in cadastro_emprestimos.emprestimo:
-        print(f"""
-    Usuário: {cadastro['usuario']}
-    Livro: {cadastro['livro']}
-    Avaliação: {cadastro['avaliacao']}
-    --------------------------------------------         
-    """)
+    >>> REGISTROS REALIZADOS: """)
+
+    if cadastro_emprestimos.emprestimo != []:
+        # retorna o registro finalizado em que cada vez ele for feito:        
+        for cadastro in cadastro_emprestimos.emprestimo:
+            print(f"""
+        Usuário: {cadastro['usuario']}
+        Livro: {cadastro['livro']}
+        Avaliação: {cadastro['avaliacao']}
+        --------------------------------------------         
+        """)
+    else:
+        print("\n*** ATENÇÃO: Ainda não houveram registros no sistema...***")
+        
